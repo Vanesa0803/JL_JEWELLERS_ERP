89099835030bbbd093cd54a0b8f49f20315e60d0
adityasolanki042006-cyber <adityasolanki042006@gmail.com>
2026-07-30 22:24:02 +0530

    Completed Database Schema

--- database/01_developer3_foundation.sql
+++ database/01_developer3_foundation.sql
@@ -1,3 +1,10 @@
-CREATE DATABASE jl_jewellers_erp;
 
 USE jl_jewellers_erp;
+CREATE TABLE backup_history (
+    backup_id INT AUTO_INCREMENT PRIMARY KEY,
+    file_name VARCHAR(255),
+    backup_type VARCHAR(50), -- manual / auto
+    file_path TEXT,
+    created_by INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
